83c438ca154219881340c651a31b5138073f90f2
Felipe Prado <rodriguesprado.felipe@gmail.com>
2015-07-28 00:14:43 -0300

    Software
    - Criação inicial do projeto seguindo o fluxograma da arquitetura de módulos do sistema.

---
+++ software/source/br.com.smom/br.com.smom.main/br.com.smom.main.root/src/main/java/br/com/smom/main/root/activator/Activator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.smom.main.root.activator;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ *
+ * @author Felipe
+ */
+public class Activator implements BundleActivator {
+    
+    public void start(BundleContext context) throws Exception {
+        //TODO add activation code here
+    }
+    
+    public void stop(BundleContext context) throws Exception {
+        //TODO add deactivation code here
+    }
+    
+}
