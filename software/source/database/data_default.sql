852815fe7d5f54c872391a8722a04b7b01e117e1
Felipe Prado <rodriguesprado.felipe@gmail.com>
2015-12-06 11:37:57 -0200

    Banco de dados
    - Atualização nos SQLs.

--- software/source/database/data_default.sql
+++ software/source/database/data_default.sql
@@ -1,9 +1,11 @@
 ﻿-- Table view_modules:
 select * from view_modules;
+
 -- INSERT INTO view_modules (type, symbolic_name, active, name, context_path, icon, position, parent) VALUES (2, 'br.com.smom.home.view', true, 'Início', '/modules/home/#/', 'fa fa-home', 1, null);
 INSERT INTO view_modules (type, symbolic_name, active, name, context_path, icon, position, parent) VALUES (1, 'br.com.smom.customer', true, 'Clientes', '#', 'fa fa-users', 2, NULL);
 INSERT INTO view_modules (type, symbolic_name, active, name, context_path, icon, position, parent) VALUES (1, 'br.com.smom.financial', true, 'Financeiro', '#', 'glyphicon glyphicon-piggy-bank', 3, NULL);
 INSERT INTO view_modules (type, symbolic_name, active, name, context_path, icon, position, parent) VALUES (1, 'br.com.smom.user', true, 'Usuários', '#', 'fa fa-user', 4, NULL);
+
 -- INSERT INTO view_modules (type, symbolic_name, active, name, context_path, icon, position, parent) VALUES (2, 'br.com.smom.customer.register', true, 'Cadastro', '/modules/customer/register/#/', 'fa fa-pencil-square-o', 1, 3);
 -- INSERT INTO view_modules (type, symbolic_name, active, name, context_path, icon, position, parent) VALUES (2, 'br.com.smom.financial.releases', true, 'Lançamentos', '/modules/financial/releases/#/', 'fa fa-money', 1, 4);
 -- INSERT INTO view_modules (type, symbolic_name, active, name, context_path, icon, position, parent) VALUES (2, 'br.com.smom.financial.reports', true, 'Relatórios', '/modules/financial/reports/#/', 'fa fa-line-chart', 2, 4);
---
+++ software/source/database/queries.sql
@@ -0,0 +1,9 @@
+﻿select datname, count(*) from pg_stat_activity group by datname;
+select * from pg_stat_activity;
+
+select datname, usename, application_name, client_addr, client_hostname, backend_start from pg_stat_activity;
+
+delete from financial_releases;
+delete from address; 
+delete from phones;
+delete from peoples;
